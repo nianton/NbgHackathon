a6a71702904638fe1b2db37723efc000489960d5
Kostas Pantos <kopantos@microsoft.com>
2018-03-15 18:02:36 +0200

    Refactored the File uploading so that it can be used in the selfie dialog as well

---
+++ NbgHackathon.Bot/Code/Helper.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace NbgHackathon.Bot
+{
+    public static class Helpers
+    {
+        public const string Passport = "Passport";
+        public const string Selfie = "Selfie";
+
+        public static async Task<Stream> GetImage(IMessageActivity message)
+        {
+            if (message.Attachments != null && message.Attachments.Any())
+            {
+                var attachment = message.Attachments.First();
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    // Skype & MS Teams attachment URLs are secured by a JwtToken, so we need to pass the token from our bot.
+                    if ((message.ChannelId.Equals("skype", StringComparison.InvariantCultureIgnoreCase) || message.ChannelId.Equals("msteams", StringComparison.InvariantCultureIgnoreCase))
+                        && new Uri(attachment.ContentUrl).Host.EndsWith("skype.com"))
+                    {
+                        var token = await new MicrosoftAppCredentials().GetTokenAsync();
+                        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                    }
+
+                    var responseMessage = await httpClient.GetAsync(attachment.ContentUrl);
+
+                    return await responseMessage.Content.ReadAsStreamAsync();
+                }
+            }
+
+            return null;
+        }
+
+        public static bool IsValidImage(Stream data)
+        {
+            return true;
+        }
+
+        public static Task StoreImage(Stream data, string container)
+        {
+            //Call Data Model
+            throw new NotImplementedException();
+        }
+    }
+}
--- NbgHackathon.Bot/Dialogs/PassportCaptureDialog.cs
+++ NbgHackathon.Bot/Dialogs/PassportCaptureDialog.cs
@@ -18,54 +18,30 @@ namespace NbgHackathon.Bot.Dialogs
             return Task.CompletedTask;
         }
 
-        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
         {
-            var message = await result;
+            var message = await result as IMessageActivity;
 
             if (message.Attachments != null && message.Attachments.Any())
             {
-                var attachment = message.Attachments.First();
-                using (HttpClient httpClient = new HttpClient())
+
+                var image = await Helpers.GetImage(message);
+
+                //await context.PostAsync($"Attachment of {attachment.ContentType} type and size of {contentLenghtBytes} bytes received.");
+
+                if (Helpers.IsValidImage(image))
                 {
-                    // Skype & MS Teams attachment URLs are secured by a JwtToken, so we need to pass the token from our bot.
-                    if ((message.ChannelId.Equals("skype", StringComparison.InvariantCultureIgnoreCase) || message.ChannelId.Equals("msteams", StringComparison.InvariantCultureIgnoreCase))
-                        && new Uri(attachment.ContentUrl).Host.EndsWith("skype.com"))
-                    {
-                        var token = await new MicrosoftAppCredentials().GetTokenAsync();
-                        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-                    }
+                    await Helpers.StoreImage(image, Helpers.Passport);
 
-                    var responseMessage = await httpClient.GetAsync(attachment.ContentUrl);
-
-                    var contentLenghtBytes = responseMessage.Content.Headers.ContentLength;
-
-                    //await context.PostAsync($"Attachment of {attachment.ContentType} type and size of {contentLenghtBytes} bytes received.");
-
-                    if (IsValidContent(responseMessage))
-                    {
-                        await StorePassportImage(responseMessage);
-                        await context.Forward<object>(new SelfieCaptureDialog(), null, context.MakeMessage());
-                    }
-
-                    await context.PostAsync("Η φωτογραφία δεν ήταν έγγυρη");
+                    await context.Forward<object>(new SelfieCaptureDialog(), this.MessageReceivedAsync, message);
                 }
-            } 
+
+                await context.PostAsync("Η φωτογραφία δεν ήταν έγγυρη");
+            }
             else
             {
                 await context.PostAsync("Παρακαλώ στείλτε μου μια φωτογραφία του διαβατηρίου σας.");
             }
-
-            context.Wait(this.MessageReceivedAsync);
-        }
-
-        private Task StorePassportImage(HttpResponseMessage responseMessage)
-        {
-            throw new NotImplementedException();
-        }
-
-        private bool IsValidContent(HttpResponseMessage responseMessage)
-        {
-            throw new NotImplementedException();
         }
     }
 }
