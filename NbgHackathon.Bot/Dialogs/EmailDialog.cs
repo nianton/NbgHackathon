feb49280210beec5a500fc32881b818185a280fb
Nikolaos Antoniou <niantoni@microsoft.com>
2018-03-16 14:11:43 +0200

    Fix signature

--- NbgHackathon.Bot/Dialogs/EmailDialog.cs
+++ NbgHackathon.Bot/Dialogs/EmailDialog.cs
@@ -25,7 +25,7 @@ namespace NbgHackathon.Bot.Dialogs
             {
                 //Persist session using Domain Model
                 var repo = Conversation.Container.Resolve<IOnboardingRepository>();
-                var persistantState = await repo.GetOrCreate(activity.Text);
+                var persistantState = await repo.GetOrCreate(activity.Text, "");
 
                 context.Call(new LegalEnityDialog(), MessageReceivedAsync);
             }
